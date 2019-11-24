b27fad944dbbcff0dd9545911e9e73546a010ab4
poggioenzo <poggio.enzo@gmail.com>
2019-11-24 23:15:09 +0100

    multifile

--- xml_txt.py
+++ xml_txt.py
@@ -10,25 +10,25 @@ TAG_TO_REMOVE = ['note', 'sic', 'hi', 'speaker', 'head', 'stage']
 
 def get_text():
     """Print cleaned verses of xml file passed in argv[1]."""
-    argv = sys.argv[1:]
-    argc = len(argv)
-    if argc == 1 and len(argv[0]) > 4 and argv[0][-4:] == '.xml':
-        with open(argv[0]) as stream:
-            lines = stream.readlines()
-            str0 = ''
-            for line in lines:
-                line = line.replace('\n', '')
-                line = line.replace('<lb', '\n<lb')
-                line = line.replace('<div', '\n<div')
-                line = line.replace('<ab', '\n<ab')
-                str0 += ' ' + line
-            soup = BeautifulSoup(str0, "xml")
-            for tag in TAG_TO_REMOVE:
-                for rem in soup(tag):
-                    rem.extract()
-            print(soup.body.get_text())
-    else:
-        print("usage: ./xml_txt.py filename.xml")
+    all_argv = sys.argv[1:]
+    for argv in all_argv:
+        if len(argv[0]) > 4 and argv[0][-4:] == '.xml':
+            with open(argv[0]) as stream:
+                lines = stream.readlines()
+                str0 = ''
+                for line in lines:
+                    line = line.replace('\n', '')
+                    line = line.replace('<lb', '\n<lb')
+                    line = line.replace('<div', '\n<div')
+                    line = line.replace('<ab', '\n<ab')
+                    str0 += ' ' + line
+                soup = BeautifulSoup(str0, "xml")
+                for tag in TAG_TO_REMOVE:
+                    for rem in soup(tag):
+                        rem.extract()
+                print(soup.body.get_text())
+        else:
+            print("usage: ./xml_txt.py filename.xml\nError file : {}\n".format(argv))
 
 
 if __name__ == '__main__':
